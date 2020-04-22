b2f6ed500f9122eb7b26e19df25a128b130667fe
akhilrajarj <raj30.akhil@gmail.com>
2020-04-22 13:30:20 +0530

    Scroll fix

--- src/GamePage/GamePage.js
+++ src/GamePage/GamePage.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, {Component}  from 'react';
 import Video from './Gamecard';
 import './GamePage.css';
-const gamePage = (props)=>{
-    return (
-        <div style={{textAlign:"center"}}>
-            <h1 style={{marginLeft:"15px",fontSize:"45px"}}>{props.match.params.id}</h1>
-            <div className="video-container">
-            <h2 className="heading">Getting Started</h2>
-             <Video source={props.location.state.video1}></Video>
-             <h2 className="heading">Tips and Tricks</h2>
-             <Video source={props.location.state.video2}></Video>
-             <h2 className="heading">Game Sensitivity</h2>
-             <Video source={props.location.state.video3}></Video>
-             </div>
-        </div>
-    );
+
+class GamePage extends Component{
+    componentDidMount(){
+        window.scrollTo(0,0);
+    }
+    render(){
+        return (
+            <div style={{textAlign:"center"}}>
+                <h1 style={{marginLeft:"15px",fontSize:"45px"}}>{this.props.match.params.id}</h1>
+                <div className="video-container">
+                <h2 className="heading">Getting Started</h2>
+                 <Video source={this.props.location.state.video1}></Video>
+                 <h2 className="heading">Tips and Tricks</h2>
+                 <Video source={this.props.location.state.video2}></Video>
+                 <h2 className="heading">Game Sensitivity</h2>
+                 <Video source={this.props.location.state.video3}></Video>
+                 </div>
+            </div>
+        );
+    }
+ 
 }
 
-export default gamePage;
+export default GamePage;
